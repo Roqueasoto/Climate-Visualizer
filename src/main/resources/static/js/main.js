73a4d7750ea516862a212a6b407dc3967518e297
Roque <roquealbertosoto@gmail.com>
2019-04-19 22:28:52 -0400

    More CSS Updates

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-    $('.header').height($(window).height());
+    $(".header").height($(window).height());
+});
+
+$(document).ready(function(){
+    $("body").css("display", "none");
+    $("body").fadeIn(1000);
+
+    $("a.transition").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(500, redirectPage);
+    })
+
+    function redirectPage(){
+        window.location = linkLocation;
+    }
 });
