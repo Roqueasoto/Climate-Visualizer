2bee8c9edee645c28a35854df43cd352c62510be
Edward Zeng <zenghua2013@gmail.com>
2019-04-28 19:53:56 -0400

    Update StatsAnalysisTester.java
    Added more graphs to test the program.

--- src/StatsAnalysisTester.java
+++ src/StatsAnalysisTester.java
@@ -10,19 +10,31 @@ public class StatsAnalysisTester {
 	public static void main(String[] args) {
 		StatsAnalysis climatedata = new StatsAnalysis(12.33, 25.66);
 
-		// Create Line Chart
+		
+		// Create Line Chart for average temperature
 		XYChart chart = QuickChart.getChart("Average Temperature for Selected Location (1900-2017)", "Year",
 				"Average Temperature", "y(x)", climatedata.getYear(), climatedata.getAvgTemp());
 		new SwingWrapper<XYChart>(chart).displayChart();
 
-		// Create Bar Chart
-		CategoryChart chart3 = new CategoryChartBuilder().width(800).height(600).title("Hotest Month Histogram")
+		// Create Bar Chart to count number of hottest month
+		CategoryChart chart2 = new CategoryChartBuilder().width(800).height(600).title("Hottest Month Histogram")
 				.xAxisTitle("Month").yAxisTitle("Count").build();
 
-		chart3.getStyler().setLegendPosition(LegendPosition.InsideNW);
-		chart3.getStyler().setHasAnnotations(true);
-		chart3.addSeries("Count", climatedata.getMonth(), climatedata.getHottestMonCount());
-		new SwingWrapper<CategoryChart>(chart3).displayChart();	
+		chart2.getStyler().setLegendPosition(LegendPosition.InsideNW);
+		chart2.getStyler().setHasAnnotations(true);
+		chart2.addSeries("Count", climatedata.getMonth(), climatedata.getHottestMonCount());
+		new SwingWrapper<CategoryChart>(chart2).displayChart();
+
+		// Create Line Chart for median hottest month for each 10 years
+		XYChart chart3 = QuickChart.getChart("Median Hottest Month for Every 10 Years (1900-2017)", "Year Range",
+				"Median Hottest Month", "y(x)", climatedata.getEachTenYearsInt(), climatedata.getMedianHottestMonth());
+		new SwingWrapper<XYChart>(chart3).displayChart();		
+
+		// Create Line Chart for average temperature of Jan to Dec
+		XYChart chart4 = QuickChart.getChart("Average Temperature by Month", "Month",
+				"Average Temperature", "y(x)", climatedata.getMonthNum(), climatedata.getMonAvgTemp());
+		new SwingWrapper<XYChart>(chart4).displayChart();
+
 	}
 	
 }
