b1d83baad4c059b2cf8b038dcde117b651609e87
Edward Zeng <zenghua2013@gmail.com>
2019-05-05 18:12:58 -0400

    Update StatsAnalysisTester.java
    Added a few more tests.

--- src/test/java/com/climate/Analysis/StatsAnalysisTester.java
+++ src/test/java/com/climate/Analysis/StatsAnalysisTester.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class StatsAnalysisTest {
-
+	
 	@Test
 	void testGetHottestMonCount() {
 		StatsAnalysis analysis = new StatsAnalysis(12.33, 25.66);
@@ -13,15 +13,15 @@ class StatsAnalysisTest {
 		assertEquals(28, (int)analysis.getHottestMonCount().get(3));
 
 		//check May
-		assertEquals(75, (int)analysis.getHottestMonCount().get(3));
+		assertEquals(75, (int)analysis.getHottestMonCount().get(4));
 		
 		//check June
-		assertEquals(15, (int)analysis.getHottestMonCount().get(3));
+		assertEquals(15, (int)analysis.getHottestMonCount().get(5));
 		
-		// check the size of the file
+		// check the size
 		assertEquals(12, analysis.getHottestMonCount().size());
 	}
-
+	
 	@Test
 	void testGetMonth() {
 		StatsAnalysis analysis = new StatsAnalysis(12.33, 25.66);
@@ -58,6 +58,41 @@ class StatsAnalysisTest {
 
 	}
 
+	@Test
+	void testGetModeHottestMonth() {
+		StatsAnalysis analysis = new StatsAnalysis(12.33, 25.66);
+		
+		//check values of 1900 and 2017
+		assertEquals(Integer.valueOf(5), analysis.getModeHottestMonth().get(1-1));
+		assertEquals(Integer.valueOf(5), analysis.getModeHottestMonth().get(12-1));
+		
+		// check the size
+		assertEquals(12, analysis.getModeHottestMonth().size());
+	}
+	
+	@Test
+	void testGetMonAvgTemp() {
+		StatsAnalysis analysis = new StatsAnalysis(12.33, 25.66);
+
+		// check values of Jan and Dec
+		assertEquals(23.29, Math.round(analysis.getMonAvgTemp().get(1 - 1) * 100.0) / 100.0);
+		assertEquals(23.77, Math.round(analysis.getMonAvgTemp().get(12 - 1) * 100.0) / 100.0);
+
+		// check the size
+		assertEquals(12, analysis.getMonAvgTemp().size());
+		
+	}
+	
+	@Test
+	void testGetMonthNum() {
+		StatsAnalysis analysis = new StatsAnalysis(12.33, 25.66);
+
+		//check month sequence;
+		for (int i = 0; i < analysis.getMonthNum().size(); i++) {
+			assertEquals(i + 1, analysis.getMonthNum().get(i).intValue());
+		}
+		
+	}
 }
 
 
